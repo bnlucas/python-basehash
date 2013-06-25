0e9d4fbd5f828e7571e3f36794a37eef98c241d8
Nathan Lucas <nathan@bnlucas.com>
2013-06-25 06:36:37 -0500

    Update base.py
    Went to a Miller-Rabin primality testing algorithm over the Fermat algorithm.

--- basehash/base.py
+++ basehash/base.py
@@ -1,19 +1,43 @@
+from random import randrange
+
 # Default hash key length
 HASH_LENGTH = 6
 
 # Prime generator, default is the golden ratio.
 GENERATOR = 1.618033988749894848
 
+# Miller-Rabin algorithm for primality testing.
+def is_prime(n, k=10):
+	if n % 2 == 0:
+		raise ValueError('An odd number must be supplied.')
+
+	def check(a, s, d, n):
+		x = pow(a, d, n)
+		if x == 1:
+			return True
+		for i in xrange(s - 1):
+			if x == n - 1:
+				return True
+			x = pow(x, 2, n)
+		return x == n - 1
+
+	s = 0
+	d = n - 1
+
+	while d % 2 == 0:
+		d >>= 1
+		s += 1
+
+	for i in xrange(k):
+		a = randrange(2, n - 1)
+		if not check(a, s, d, n):
+			return False
+	return True
+
 # Generates next prime from supplied `base` and `n`.
 # The default `gen` is set to the golden ratio, golden primes.
 def prime(base, n, gen=GENERATOR):
 	def next_prime(num):
-		def is_prime(x):
-			if x == 2:
-				return True
-			if not(x & 1):
-				return False
-			return pow(2, x-1, x) == 1
 		if (num % 2 == 0) and (num != 2):
 			num += 1
 		while True:
