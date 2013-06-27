037738f20618cfbe226f50bd2f04df7b53f68623
Nathan Lucas <nathan@bnlucas.com>
2013-06-27 07:41:40 -0600

    Update base.py
    Changed the Miller-Rabin primality test to the Baillie-PSW primaility test. Updated a few code pieces in mulinv(x, mod).

--- basehash/base.py
+++ basehash/base.py
@@ -1,4 +1,4 @@
-from random import randrange
+from primes import next_prime
 
 # Default hash key length
 HASH_LENGTH = 6
@@ -6,45 +6,9 @@ HASH_LENGTH = 6
 # Prime generator, default is the golden ratio.
 GENERATOR = 1.618033988749894848
 
-# Miller-Rabin algorithm for primality testing.
-def is_prime(n, k=10):
-	if n % 2 == 0:
-		raise ValueError('An odd number must be supplied.')
-
-	def check(a, s, d, n):
-		x = pow(a, d, n)
-		if x == 1:
-			return True
-		for i in xrange(s - 1):
-			if x == n - 1:
-				return True
-			x = pow(x, 2, n)
-		return x == n - 1
-
-	s = 0
-	d = n - 1
-
-	while d % 2 == 0:
-		d >>= 1
-		s += 1
-
-	for i in xrange(k):
-		a = randrange(2, n - 1)
-		if not check(a, s, d, n):
-			return False
-	return True
-
 # Generates next prime from supplied `base` and `n`.
 # The default `gen` is set to the golden ratio, golden primes.
 def prime(base, n, gen=GENERATOR):
-	def next_prime(num):
-		if (num % 2 == 0) and (num != 2):
-			num += 1
-		while True:
-			if is_prime(num):
-				break
-			num += 2
-		return num
 	return next_prime(int(base ** n * gen))
 
 
@@ -54,18 +18,12 @@ def mulinv(x, mod):
 		raise ValueError('Modulus must be greater than zero.')
 	a = abs(x)
 	b = mod
-	c = f = 1
-	d = e = 0
+	c, d, e, f = 1, 0, 0, 1
 	while b > 0:
 		q, r = divmod(a, b)
 		g = c - q * d
 		h = e - q * f
-		a = b
-		b = r
-		c = d
-		d = g
-		e = f
-		f = h
+		a, b, c, d, e, f = b, r, d, g, f, h
 	if a != 1:
 		raise ValueError('{x} has no multiplicative '
 			'inverse modulo {m}'.format(x=x, m=mod))
