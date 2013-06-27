d43e2cb93ec5870d47a35f2b8a35440b037c7d5f
Nathan Lucas <nathan@bnlucas.com>
2013-06-27 08:12:52 -0600

    removed miller-rabin code.

--- basehash/base.py
+++ basehash/base.py
@@ -6,33 +6,6 @@ HASH_LENGTH = 6
 # Prime generator, default is the golden ratio.
 GENERATOR = 1.618033988749894848
 
-# Miller-Rabin algorithm for primality testing.
-def is_prime(n, k=10):
-	if n % 2 == 0:
-		raise ValueError('An odd number must be supplied.')
-
-	def check(a, s, d, n):
-		x = pow(a, d, n)
-		if x == 1:
-			return True
-		for i in xrange(s - 1):
-			if x == n - 1:
-				return True
-			x = pow(x, 2, n)
-		return x == n - 1
-
-	s = 0
-	d = n - 1
-
-	while d % 2 == 0:
-		d >>= 1
-		s += 1
-
-	for i in xrange(k):
-		a = randrange(2, n - 1)
-		if not check(a, s, d, n):
-			return False
-	return True
 
 # Generates next prime from supplied `base` and `n`.
 # The default `gen` is set to the golden ratio, golden primes.
