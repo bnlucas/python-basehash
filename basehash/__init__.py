e4614653d332a065614ab4409938fae735a7e66e
Nathan Lucas <nathan@bnlucas.com>
2013-06-28 13:35:24 -0600

    setup.py overhaul.

--- basehash/__init__.py
+++ basehash/__init__.py
@@ -1,1 +1,3 @@
 __all__ = ('base36', 'base52', 'base56', 'base58', 'base62', 'base94')
+
+__version__ = '1.0.6'
--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 import os
 import sys
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import basehash
 
 
 if sys.argv[-1] == 'publish':
@@ -9,18 +14,29 @@ if sys.argv[-1] == 'publish':
     sys.exit()
 
 
+if sys.argv[-1] == 'prep':
+    os.system('python setup.py sdist')
+    sys.exit()
+
+
 setup(
     name='BaseHash',
-    packages=['basehash'],
-    version='1.0.5',
-    description='Reversible obfuscated identifier hashes to a given base and length.',
+    version=basehash.__version__,
+    description='Reversible obfuscated identifier hashes.',
+    long_description=open('README.md').read() + '\n\n' + open('HISTORY.md').read(),
     author='Nathan Lucas',
     author_email='nathan@bnlucas.com',
     url='http://bnlucas.github.io/python-basehash',
     download_url='https://github.com/bnlucas/python-basehash/archive/master.zip',
-    package_data={'': ['LICENSE', 'HISTORY.rst']},
+    packages=['basehash'],
+    package_data={'': ['LICENSE'], 'tests': '*.py'},
+    package_dir={'basehash': 'basehash'},
+    include_package_data=True,
+    install_requires=[],
+    license=open('LICENSE').read(),
+    zip_safe=False,
     keywords=['base', 'encoding', 'hash', 'hashing', 'security'],
-    classifiers=[
+    classifiers=(
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python',
         'License :: OSI Approved :: Apache Software License',
@@ -30,17 +46,5 @@ setup(
         'Topic :: Security',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Utilities'
-    ],
-    long_description='''
-BaseHash - Reversible obfuscated identifier hashing.
-----------------------------------------------------
-
-BaseHash comes with multiple base hashing packages, and is also extendible. use
-this package to quickly hash integers to a given base and length.
-
-- Base36, Base52, Base56, Base58, Base62, Base94.
-- Extend with ease.
-- Relies on primes base^len, example: base62 hash, 6 digits, uses
-  (62 ** 6) * 1.618033988749894848, finds next highest prime.
-'''
+    ),
 )
